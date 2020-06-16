1e91e773263b13329e8d2f5aea9a70c752fcb514
Tebeica <tebeicat@yahoo.com>
2020-06-16 08:37:00 -0600

    added a few more filters

--- kijiji_scraper.py
+++ kijiji_scraper.py
@@ -13,26 +13,51 @@ I_DONT_WANT_IT = ['entourage', 'Entourage', 'Rondo', 'rondo',
             'CRV', 'crv', 'caravan', 'Caravan', 'Patriot', 'GMC',
             'MDX', 'RDX', 'Cobalt', 'Cavalier', 'Journey', 'Quest',
             'Tribute', 'Yaris', 'yaris', 'Equinox', 'Elantra',
-            'Versa', 'Escape', 'Veracruz', 'Sonata', 'STS', 
+            'Versa', 'Escape', 'Veracruz', 'Sonata', 'STS', 'F-150', 
             'Grand', 'Yukon', 'HHR', 'Pathfinder', 'F150', 'CR-V',
             'Camry', 'accent', 'patriot', 'E350', 'E250', 'silverado',
             'deville', 'Deville', 'Uplander', 'uplander', 'Suburban',
-            'Liberty', 'minivan', 'Rogue', 'Edge', 'Santa Fe',
-            'Fusion', 'Traverse', 'Buick', 'Prius', 'Sunfire']
+            'Liberty', 'minivan', 'Rogue', 'Edge', 'Santa Fe', 'grand',
+            'Fusion', 'Traverse', 'Buick', 'Prius', 'Sunfire', 
+            'Highlander', '4Runner', 'Tacoma', 'Sebring', 'f150',
+            'ELANTRA', 'spectra', 'compass', 'Spectra']
             
 IDONTWANTOMETER = 0
 
+I_REALLY_WANT_IT = ['335', 'Evo', 'evo', 'G35S', 'g35', 'G35',
+                    'Aristo', 'jdm', 'JDM', 'Jdm', 'S4', 's4',
+                    '535', '540', '550', '545', 'e36', 'E36',
+                    'STI', 'sti', 'STi', 'Supra', '335i', '335xi',
+                    '535i', '535xi', 'MazdaSpeed', 'MAZDASPEED',
+                    'Mazdaspeed3', 'Mazdaspeed6', 'MazdaSpeed3',
+                    'MazdaSpeed6', 'WRX', 'TT', 'skyline', 'GTI',
+                    'Skyline', 'Mustang', '350z', 'LS400', 'LS430',
+                    'IS250', 'IS350', 'is350', 'is250', 'ls430',
+                    'GLI', '20th Anniversary', '20th anniversary',
+                    'GT-S', 'GTS', 'Saab', '240sx', '540i', '545i',
+                    '550i', 'wrx', '530i', '530', 'Type S', 'type S',
+                    'type-s', 'Type-S', 'Genesis', 'Legacy', 'legacy',
+                    'type s', 'gts']
+
+IWANTOMETER = 0
 
 a_url = 'https://www.kijiji.ca/b-cars-trucks/alberta/new__used/page-'
 b_url = '/c174l9003a49?price-type=fixed&price=1500__8000&for-sale-by=ownr'
 
+
+# main file containing most ads
 filename = "kijiji_scrape.csv"
 f = open(filename, "w+")
-header = "BRAND, PRICE, LOCATION, DETAILS, LINK \n"
+header = "BRAND, PRICE, DETAILS, LOCATION, LINK \n"
 f.write(header)
 
+# secondary file containing the ones matching I_REALLY_WANT_IT
+secondary = "peak_interest.csv"
+s = open(secondary, "w+")
+s.write(header)
 
-for i in range(1,10):
+
+for i in range(1,15):
     my_url = a_url + str(i) + b_url
 
     uClient = uReq(my_url)
@@ -49,8 +74,9 @@ for i in range(1,10):
         for model in I_DONT_WANT_IT:
             if model in brand:
                 IDONTWANTOMETER = 1
+        # skip the models that I don't want
         if IDONTWANTOMETER == 1:
-            print('found match ' + brand + ', skipping...')
+            print('Found bad match ' + brand + ', skipping... \n')
             IDONTWANTOMETER = 0
             continue
 
@@ -74,8 +100,18 @@ for i in range(1,10):
         print("LINK: " + link)
         print('\n')
 
-        f.write(brand.replace('\n', '').replace(',', '|') + ',' + price.replace(',','.') + ',' + details.replace(',','.') + ',' + location + ',' + link + '\n')
+        for model in I_REALLY_WANT_IT:
+            if model in brand:
+                IWANTOMETER = 1
+        if IWANTOMETER == 1:
+            s.write(brand.replace('\n', '').replace(',', '|') + ',' + price.replace(',','.') + ',' + details.replace(',','.') + ',' + location + ',' + link + '\n')
+            print('Found good match ' + brand + ', adding to peak_interest... \n')
+            IWANTOMETER = 0
+        else:
+            f.write(brand.replace('\n', '').replace(',', '|') + ',' + price.replace(',','.') + ',' + details.replace(',','.') + ',' + location + ',' + link + '\n')
 
+    s.write('\n')
     f.write('\n')
 
 f.close()
+s.close()
